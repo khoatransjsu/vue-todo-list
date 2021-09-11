373d20ee19929f45efb3dcebca3eec2e11ef7df8
khoa <54516445+khoatransjsu@users.noreply.github.com>
2021-09-10 17:24:45 -0700

    Todo Reminder update PUT request

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -37,8 +37,20 @@
           res.status === 200 ? (this.tasks = this.tasks.filter((task)=> task.id !== id)) : alert('Cannot delete task')
         }
       },
-      toggleReminder(id){
-        this.tasks = this.tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder} : task)
+      async toggleReminder(id){
+          const taskToToggle = await this.fetchTask(id)
+          const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+          const res = await fetch(`api/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-type': 'application/json',
+             },
+            body: JSON.stringify(updTask),
+          })
+        const data = await res.json()
+          this.tasks = this.tasks.map((task) =>
+          task.id === id ? { ...task, reminder: data.reminder } : task
+        )
       },
       async addTask(task){       
         const res = await fetch('api/tasks',{

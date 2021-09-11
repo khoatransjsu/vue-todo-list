aa4a923fa33f287642711c42d6a845523e947938
khoa <54516445+khoatransjsu@users.noreply.github.com>
2021-09-10 17:16:46 -0700

    Update post and delete

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -30,20 +30,40 @@
       }
     },
     methods:{
-      deleteTask(id){
-        this.tasks = this.tasks.filter((task)=> task.id !== id)
+      async deleteTask(id){
+        if(confirm('Are you sure?')){
+          const res = await fetch(`api/tasks/${id}`,{
+            method: 'DELETE'})
+          res.status === 200 ? (this.tasks = this.tasks.filter((task)=> task.id !== id)) : alert('Cannot delete task')
+        }
       },
       toggleReminder(id){
         this.tasks = this.tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder} : task)
       },
-      addTask(task){
-        this.tasks=[...this.tasks, task]
+      async addTask(task){       
+        const res = await fetch('api/tasks',{
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+            
+          },
+          body: JSON.stringify(task)
+        })
+
+        const data = await res.json();
+        
+        this.tasks=[...this.tasks, data]
       },
       toggleAddTask(){
         this.showAddTask = !this.showAddTask
       },
       async fetchData(){
-        const res = await fetch('http://localhost:5000/tasks')
+        const res = await fetch('api/tasks')
+        const data = await res.json();
+        return data;
+      },
+      async fetch(id){
+        const res = await fetch(`api/tasks/${id}`)
         const data = await res.json();
         return data;
       }
